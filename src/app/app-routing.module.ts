ed65212ecc9af7721d8a0dfc6a7335061297015e
Mateus Henrique Gonçalves <Mateus_hg@hotmail.com>
2022-09-15 13:49:52 +0200

    rotas ok!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './navmanu/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
 
 ];
 
