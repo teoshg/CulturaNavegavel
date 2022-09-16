75216e43508c9f66f380e22cec93215af2ecd211
Mateus Henrique Gonçalves <Mateus_hg@hotmail.com>
2022-09-16 04:19:50 +0200

    adicionando componentes

--- src/app/navmanu/menu.module.ts
+++ src/app/navmanu/menu.module.ts
@@ -1,5 +1,5 @@
-import { ViajensComponent } from './viajens/viajens.component';
 import { HomeComponent } from './home/home.component';
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
@@ -9,7 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     HomeComponent,
-    ViajensComponent
+    
   ],
   imports: [
     CommonModule,
